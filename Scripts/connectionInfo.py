f7f453d89079195ba5f9b5f68cb65e251656e54f
Sathish <sathish@kapiital.com>
2026-01-20 16:26:26 +0530

    updated for folder structure

--- Scripts/connectionInfo.py
+++
@@ -1,9 +0,0 @@
-import psycopg2
-
-conn = psycopg2.connect(
-    host="localhost",   # prefer 127.0.0.1 over "localhost" for TCP
-    port=5432,
-    user="postgres",
-    password="Password#1234",
-    dbname="stockAnalysis"
-)
