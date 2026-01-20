ad0fb8c59fb5663119edc3228a12693a1bcbc6c1
Sathish <sathish@kapiital.com>
2026-01-20 16:53:31 +0530

    changes made for project run properly

--- clientEnv/Scripts/activate.bat
+++ clientEnv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set "VIRTUAL_ENV=C:\MAMP\htdocs\Data-Driven-StockAnalysis\clientEnv"
+set "VIRTUAL_ENV=C:\MAMP\htdocs\dataDrivenStockAnalysis\clientEnv"
 
 if not defined PROMPT set PROMPT=$P$G
 
--- clientEnv/Scripts/activate.fish
+++ clientEnv/Scripts/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV 'C:\MAMP\htdocs\Data-Driven-StockAnalysis\clientEnv'
+set -gx VIRTUAL_ENV 'C:\MAMP\htdocs\dataDrivenStockAnalysis\clientEnv'
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"Scripts $PATH
